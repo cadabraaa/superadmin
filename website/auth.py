845ae9b2685ee7f7742c376ecb9c55ccb365c774
cadabraaa <151050186+cadabraaa@users.noreply.github.com>
2024-03-04 06:44:53 +0000

    applied validations

--- website/auth.py
+++ website/auth.py
@@ -3,13 +3,12 @@ from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from flask_login import login_user, login_required, logout_user, current_user
+import re
 
-auth= Blueprint('auth', __name__)
+auth = Blueprint('auth', __name__)
 
 
-
-
-@auth.route('/sign_up', methods=['GET', 'POST'] )
+@auth.route('/sign_up', methods=['GET', 'POST'])
 def sign_up():
   if request.method == 'POST':
     company_code = request.form.get('company_code')
@@ -21,23 +20,26 @@ def sign_up():
     password2 = request.form.get('password2')
 
     user = User.query.filter_by(email=email).first()
+    c_code = User.query.filter_by(company_code=company_code).first()
 
     if user:
-        flash('Email already exists.', category='error')
-    elif len(email) < 4:
-        flash('Email must be greater than 3 characters.', category='error')
-    elif password1 != password2:
-        flash('Passwords don\'t match.', category='error')
-    elif len(password1) < 7:
-        flash('Password must be at least 7 characters.', category='error')
+      flash('Email already exists.', category='error')
+    elif c_code:
+        flash('The company code already exists.', category='error')
+    elif not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+      flash('Email is not valid.', category='error')
     else:
-        new_user = User(email=email, company_code=company_code, company_name=company_name, mobile=mobile, password=generate_password_hash(
-            password1, method='pbkdf2:sha256'))
-        db.session.add(new_user)
-        db.session.commit()
-        login_user(new_user, remember=True)
-        flash('Account created!', category='success')
-        return redirect(url_for('views.home'))
+      new_user = User(email=email,
+                      company_code=company_code,
+                      company_name=company_name,
+                      mobile=mobile,
+                      establishment_year=establishment_year,
+                      password=generate_password_hash(password1,
+                                                      method='pbkdf2:sha256'))
+      db.session.add(new_user)
+      db.session.commit()
+      login_user(new_user, remember=True)
+      flash('Account created!', category='success')
+      return redirect(url_for('views.home'))
 
   return render_template("sign_up.html", user=current_user)
-
